268ad9ec3fc3acb3ea1b62d4814549583d7db2d6
r3flexmlg <r3flexmlg@gmail.com>
2022-03-26 14:24:23 +0100

    s

--- tibiagraphs/settings.py
+++ tibiagraphs/settings.py
@@ -89,8 +89,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=1200)
-DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
