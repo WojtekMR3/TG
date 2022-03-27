2550c7d31fb33b14db75e56e6313fa5694ed24ea
r3flexmlg <r3flexmlg@gmail.com>
2022-03-27 15:00:01 +0200

    s

--- tibiagraphs/settings.py
+++ tibiagraphs/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'tibiagraphs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'CONN_MAX_AGE': 500
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'test',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#         'CONN_MAX_AGE': 500
+#     }
+# }
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
