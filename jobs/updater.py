73d6cec84d0d58a0fae7de47d92926a2579eabb0
r3flexmlg <r3flexmlg@gmail.com>
2022-03-31 15:55:37 +0200

    s

--- jobs/updater.py
+++ jobs/updater.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import schedule_api
 from .tbapi import get_hs_as
 
 def start():
@@ -8,5 +7,5 @@ def start():
   #scheduler.add_job(schedule_api, 'cron', second=0)
   #scheduler.add_job(get_hs_as, 'cron', second=5)
   #scheduler.add_job(get_hs_as, 'cron', minute=50)
-  scheduler.add_job(get_hs_as, 'cron', hour=16)
+  scheduler.add_job(get_hs_as, 'cron', second=30)
   scheduler.start()
